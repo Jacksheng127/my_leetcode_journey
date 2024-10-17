442330edc31440615f76d3d69d5eac9dc188a9f9
Jacksheng127 <limjacksheng127@gmail.com>
2024-10-17 22:40:36 +0800

    remove command on leetcode 921

--- leetcode_921/MinimumAddToMakeParenthesesValid.py
+++ leetcode_921/MinimumAddToMakeParenthesesValid.py
@@ -1,46 +1,21 @@
-# class MinimumAddToMakeParenthesesValid:
-#     def min_add_to_make_valid(self, s: str) -> int:
-#         leftCount, rightCount = 0, 0
-#         for c in s:
-#             if c == '(':
-#                 leftCount += 1
-#             elif leftCount > 0:
-#                 leftCount -= 1
-#             else:
-#                 rightCount += 1
-#         return leftCount + rightCount
+class MinimumAddToMakeParenthesesValid:
+    def min_add_to_make_valid(self, s: str) -> int:
+        leftCount, rightCount = 0, 0
+        for c in s:
+            if c == '(':
+                leftCount += 1
+            elif leftCount > 0:
+                leftCount -= 1
+            else:
+                rightCount += 1
+        return leftCount + rightCount
 
 
-# def main():
-#     main_instance = MinimumAddToMakeParenthesesValid()
-#     print(main_instance.min_add_to_make_valid("())"))
-#     print(main_instance.min_add_to_make_valid("((("))
+def main():
+    main_instance = MinimumAddToMakeParenthesesValid()
+    print(main_instance.min_add_to_make_valid("())"))
+    print(main_instance.min_add_to_make_valid("((("))
 
 
-# if __name__ == "__main__":
-#     main()
-
-
-
-# z = set('abc')
-# z.add('san')
-# z.update(set(['p', 'q']))
-# print(z)
-    
-
-print(any([2>8, 4>2, 1>2]))
-
-# name = "snow storm"
-# print(name[2:7:2])
-
-# def add(i, values = []):
-#     values.append(i)
-#     return values
-# add(1)
-# add(2)
-# add(3)
-# print(add(4))
-
-# m=[1,2,3]
-# tupl=('Python',)*(m.__len__() - m[::-1][0])
-# print(tupl)
+if __name__ == "__main__":
+    main()
