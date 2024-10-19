796a880e507935fb058701c610eef98be043c46d
Jacksheng127 <limjacksheng127@gmail.com>
2024-10-20 00:45:02 +0800

    leetcode 1545

---
+++ leetcode_1545/FindKthBitInNthBinaryString.py
@@ -0,0 +1,25 @@
+class FindKthBitInNthBinaryString:
+    def findKthBit(self, n: int, k: int) -> str:
+        length = 2 ** n - 1
+        
+        def helper(length, k):
+            if length == 1:
+                return "0"
+            
+            half = length // 2
+            if k <= half:
+                return helper(half, k)
+            elif k > half + 1:
+                res = helper(half, 1 + length - k)
+                return "0" if res == "1" else "1"
+            else:
+                return "1"
+        
+        return helper(length, k)
+
+def main():
+    main_instance = FindKthBitInNthBinaryString()
+    print(main_instance.findKthBit(3, 1))
+
+if __name__ == "__main__":
+    main()
